cd741ff83a8625eb3c54db4447ceeacaab9997d3
fred carella <fred.carela@gmailcom>
2026-03-19 15:13:40 -0400

    commit

---
+++ src/test/java/csd230/bookstore/ApplicationTest.java
@@ -0,0 +1,12 @@
+package csd230.bookstore;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+    @Test
+    void name() {
+        assert(false);
+    }
+}
